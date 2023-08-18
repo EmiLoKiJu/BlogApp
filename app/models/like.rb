93223625e6645be2f8cc171262274c55f2e01518
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-08-18 09:36:39 -0800

    Revert "Add data to database, and custom methods to tables"
    This reverts commit 67c190ac55e6070151348f1137cf2971efbd328c.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,4 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
-
-  after_create :update_post_likes_counter
-  after_destroy :update_post_likes_counter
-
-  def update_post_likes_counter
-    post.update(likes_counter: post.likes.count)
-  end
 end
