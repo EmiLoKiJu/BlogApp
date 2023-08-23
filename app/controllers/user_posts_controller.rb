e13120aeca7bb5fe94c92ee6a8685663776b9b90
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-08-22 17:58:35 -0800

    Add styles, links and data to webpages

--- app/controllers/user_posts_controller.rb
+++ app/controllers/user_posts_controller.rb
@@ -1,7 +1,12 @@
 class UserPostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts_per_page = 2
+    @total_pages = (@user.posts.count.to_f / @posts_per_page).ceil
+    @page = (params[:page] || 1).to_i
+    offset = (@page - 1) * @posts_per_page
+    @posts = @user.posts.limit(@posts_per_page).offset(offset)
+    @users = User.all
   end
 
   def show
