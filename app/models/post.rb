5e802cc0a330c6e3cca5fa49a0347ba0946773b4
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-08-23 13:12:30 -0800

    Made requiered changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,7 +16,7 @@ class Post < ApplicationRecord
     author.update(posts_counter: author.posts.count)
   end
 
-  def recent_comments(limit = 5)
-    comments.order(created_at: :desc).limit(limit)
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 end
