22d45246774cebfe8bc13c1f9caafad4ff266db4
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-08-25 15:02:28 -0800

    Add styles to forms

--- app/controllers/user_post_comments_controller.rb
+++ app/controllers/user_post_comments_controller.rb
@@ -10,12 +10,10 @@ class UserPostCommentsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:post_id])
     @comment = @post.comments.build(comment_params.merge(author: current_user))
-    puts @comment.errors.full_messages
     if @comment.save
-      puts @comment.errors.full_messages
       redirect_to user_post_path(@user, @post), notice: 'Post created successfully.'
     else
-      puts @comment.errors.full_messages
+      # puts @comment.errors.full_messages
       redirect_to new_user_post_comment_path(@user, @post), alert: 'There was an error creating the comment.'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
-# Remplazed frozen_string_literal true for something else
-
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create], controller: 'user_posts' do
-      resources :comments, only: [:new, :create], controller: 'user_post_comments' do
-        resources :likes, only: [:new, :create], controller: 'user_post_comments_likes'
-      end
+      resources :comments, only: [:new, :create], controller: 'user_post_comments'
+      resources :likes, only: [:create], controller: 'user_post_likes'
     end
   end
 end
