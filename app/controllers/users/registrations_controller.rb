f7b0a7f0ac494a31793cff6d08d9208b056a0211
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-08-30 19:00:39 -0800

    Fix linters

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -32,13 +32,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def cancel
   #   super
   # end
+
   protected
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
   end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
