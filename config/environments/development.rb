d7d08b6f41f208058247f15116a7425cf52ce398
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-08-30 18:34:30 -0800

    Add a feature to log in without confirming mail within 1 hour

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # Devise
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
